a786c6c3766f7611347f4c9055391bbd2ebd0677
Akshat Sharma <webdevakshat@gmail.com>
2023-07-11 18:22:38 +0530

    Created Navbar Component And Added Some Icons

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,21 @@
+import BrandLogo from "../assets/ecoplumeLogo.png";
+import { BiMenu } from "react-icons/bi";
+import { CiSearch } from "react-icons/ci";
 
 const Navbar = () => {
   return (
     <>
-    
+      <nav className="mt-1 flex justify-between">
+        <div className="mobileNavToggle"><img src={BiMenu} alt="Menu Toggler" /></div>
+        <header>
+          <div className="brand-name">
+            <img src={BrandLogo} className="h-14" alt="Logo Of EcoPlume" />
+          </div>
+        </header>
+        
+      </nav>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
