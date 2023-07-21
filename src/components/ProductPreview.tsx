86c2a6e95f5aea7b25545b0683fc7a323390e655
Akshat Sharma <webdevakshat@gmail.com>
2023-07-21 15:55:48 +0530

    Started building ProductPreview Page

---
+++ src/components/ProductPreview.tsx
@@ -0,0 +1,16 @@
+
+type ProductPreviewProps = {
+    category : string
+    product : string
+    productName : string
+}
+
+const ProductPreview = ({category , product , productName} : ProductPreviewProps) => {
+  return (
+    <>
+    <div>Product Preview</div>
+    </>
+  )
+}
+
+export default ProductPreview
