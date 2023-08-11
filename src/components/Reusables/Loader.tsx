789d8829dea6c9aea68c4dea8067bb34c8f45cbe
Akshat Sharma <webdevakshat@gmail.com>
2023-08-11 18:49:49 +0530

    Added Dark Styles For Loader

--- src/components/Reusables/Loader.tsx
+++ src/components/Reusables/Loader.tsx
@@ -3,10 +3,10 @@ import loaderImg from "../../assets/loader.png";
 const Loader = () => {
   return (
     <>
-      <div className="bg-white flex justify-center items-center w-screen h-screen absolute top-0 z-40">
+      <div className="bg-white flex justify-center items-center w-screen h-screen absolute top-0 z-40 dark:bg-black">
         <img
           src={loaderImg}
-          className="h-24 animate-load"
+          className="h-24 animate-load dark:invert"
           alt="Loading Image Animation"
         />
       </div>
