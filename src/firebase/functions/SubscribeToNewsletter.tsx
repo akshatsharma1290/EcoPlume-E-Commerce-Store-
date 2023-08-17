52756e0384dd679c3278f241b41a80457a816cbd
Akshat Sharma <webdevakshat@gmail.com>
2023-08-17 19:15:26 +0530

    Fix : Newsletter Firestore Id. Using the user's auth id instead of his name.

--- src/firebase/functions/SubscribeToNewsletter.tsx
+++ src/firebase/functions/SubscribeToNewsletter.tsx
@@ -1,13 +1,18 @@
-import { collection, doc, setDoc } from "firebase/firestore"
-import { firestore } from "../firebase"
+import { collection, doc, setDoc } from "firebase/firestore";
+import { auth, firestore } from "../firebase";
 
-export const subscribeToNewsletter = async  (name : string , email : string , review : string) => {
-    try {
-        const docRef = doc(collection(firestore , "subscribed_users") , name)
-        const data = {name , email , review}
+export const subscribeToNewsletter = async (
+  name: string,
+  email: string,
+  review: string
+) => {
+  try {
+    const uid = auth.currentUser?.uid;
+    const docRef = doc(collection(firestore, "subscribed_users"), uid);
+    const data = { name, email, review };
 
-        await setDoc(docRef , data)
-    } catch (error) {
-        console.error(error , "Failed Subscribing")
-    }
-}
+    await setDoc(docRef, data);
+  } catch (error) {
+    console.error(error, "Failed Subscribing");
+  }
+};
