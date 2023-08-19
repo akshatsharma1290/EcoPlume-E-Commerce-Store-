9c6b21fe8029b281fb57b1b73e6ede05109c3d5d
Akshat Sharma <webdevakshat@gmail.com>
2023-08-19 20:13:39 +0530

    Added Loader On Checkout Panel.

--- src/components/CheckoutComponents/CheckoutPanel.tsx
+++ src/components/CheckoutComponents/CheckoutPanel.tsx
@@ -1,15 +1,27 @@
+import { useAppSelector } from "../../store/hooks";
+import { loadingSelector } from "../../store/slices/loadingSlice";
 import StripeBuyButton from "./BuyButton";
+import Loader from "../Reusables/Loader.tsx";
 
 const CheckoutPanel = () => {
+  const isLoading = useAppSelector(loadingSelector);
   return (
-    <section className="flex flex-col justify-center items-center h-screen dark:text-white">
-      <StripeBuyButton />
-        <article className="text-center font-outfit font-medium px-2 mt-4">
+    <>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <section className="flex flex-col justify-center items-center h-screen dark:text-white">
+          <StripeBuyButton />
+          <article className="text-center font-outfit font-medium px-2 mt-4">
             <p>Kindly Fill Any Amount.</p>
-            <p>This is a test payment you can use a test card to see it working</p>
+            <p>
+              This is a test payment you can use a test card to see it working
+            </p>
             <p>Payment Intents And Live Mode Will Be Integerated Soon.</p>
-        </article>
-    </section>
+          </article>
+        </section>
+      )}
+    </>
   );
 };
 
