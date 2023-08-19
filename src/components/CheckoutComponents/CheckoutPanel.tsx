fe855f6afda364fcc077e222b32de443071b7158
Akshat Sharma <webdevakshat@gmail.com>
2023-08-19 11:40:36 +0530

    Added Some Points On Checkout Panel.

--- src/components/CheckoutComponents/CheckoutPanel.tsx
+++ src/components/CheckoutComponents/CheckoutPanel.tsx
@@ -3,11 +3,12 @@ import StripeBuyButton from "./BuyButton";
 const CheckoutPanel = () => {
   return (
     <section className="flex flex-col justify-center items-center h-screen">
-        <article className="text-center font-outfit font-medium">
-            <p>Kindly Fill Any Amount.</p>
-            <p>This Is Just A Test Payment.</p>
-        </article>
       <StripeBuyButton />
+        <article className="text-center font-outfit font-medium px-2">
+            <p>Kindly Fill Any Amount.</p>
+            <p>This is a test payment you can use a test card to see it working</p>
+            <p>Payment Intents And Live Mode Will Be Integerated Soon.</p>
+        </article>
     </section>
   );
 };
