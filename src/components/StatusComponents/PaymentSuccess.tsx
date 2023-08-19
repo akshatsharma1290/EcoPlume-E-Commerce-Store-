0f13d90efcf34f4a77dc5cc37abf9ff9dec4af5d
Akshat Sharma <webdevakshat@gmail.com>
2023-08-19 14:31:46 +0530

    Success Page Designing Done.

--- src/components/StatusComponents/PaymentSuccess.tsx
+++ src/components/StatusComponents/PaymentSuccess.tsx
@@ -1,10 +1,11 @@
-
+import SuccessGif from "../../assets/success.gif"
 
 const PaymentSuccess = () => {
   return (
     <>
-    <section className="mt-24">
-        Payment Success
+    <section className="flex flex-col justify-center items-center h-screen bg-[#2ecc71]">
+       <img src={SuccessGif} alt="Payment Success Animation" />
+       <h1 className="text-white font-outfit tracking-wide font-bold text-4xl mt-2">Payment Successful</h1>
     </section>
     </>
   )
